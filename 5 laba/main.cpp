ac493eeb7554a0f5a0008f9289778bca4dabc08f
th6915th-ux <th6915th@gmail.com>
2026-01-02 18:09:12 +0300

    каркас

---
+++ 5 laba/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+struct parrots
+{
+    int mass;
+    wstring color;
+    wstring species;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    //ф-я 1:
+
+    //ф-я 2:
+
+    //ф-я 3:
+
+}
